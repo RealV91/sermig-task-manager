f8fdf7fcd96b27ad71c1aa34d21ef719bf022a7e
Alessio Vautero <alessio@example.com>
2025-07-11 22:43:09 +0200

    Progetto Sermig Task Manager

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
-// tailwind.config.js
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
